a70067a848ef2b2d7d8aac58dd2bcd464550d005
silence-pioneer <3094226353@qq.com>
2026-05-09 19:39:17 +0800

    class1night

--- design7.c
+++ design7.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+int result(int *arr, int n, int k){
+    if(n <= 0) return -1;
+    int min = arr[0];
+    int s=0;
+    for(int i = 1; i < n; i++){
+        if(arr[i] < min){
+            min = arr[i];
+            s = i;
+        }
+    }
+    if(k == 1) return min;
+    arr[s] = arr[n-1]; 
+    return result(arr, n-1, k-1);
+}
 int main(){
+    int n,k;
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n * sizeof(int));
+    if(arr == NULL) return 0;
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+    if(k > n || k <= 0) {
+        printf("-1");
+        free(arr);
+        return 0;
+    }
+    printf("%d", result(arr, n, k));
+    free(arr);
     return 0;
 }
